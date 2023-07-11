1e04e5b54f960cce5a28f54b6b1c6dee101bb6fa
day-6 <138205816+day-6@users.noreply.github.com>
2023-07-11 14:21:51 +0530

    Initial commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  const d = new Date();
+  let year = d.getFullYear();
+
+  return (
+    <footer>
+      <p>Copyright ⓒ {year} </p>
+    </footer>
+  );
+}
+
+export default Footer;
